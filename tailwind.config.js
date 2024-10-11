e00e66d25a8afb4f77c58340ba4a3feb03a17f33
SonLeanCode <12c3tvknguyenvanson@gmail.com>
2024-10-11 09:32:40 +0700

    fix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const flowbite = require("flowbite-react/tailwind");
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}" // Thêm đường dẫn này
+  ],
   theme: {
     extend: {
       fontFamily: {
         kanit: ["Kanit", "sans-serif"],
       },
-      
     },
   },
-  plugins: []
+  plugins: [
+    require("flowbite/plugin"), // Sử dụng flowbite plugin
+  ],
 };
