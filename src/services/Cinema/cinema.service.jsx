f68bc6567c19f0ac528c6898d8a0fbef7c60a95a
duy <vdntduy0410@gmail.com>
2024-10-24 23:42:08 +0700

    feat: getCinemasByRegionId

--- src/services/Cinema/cinema.service.jsx
+++ src/services/Cinema/cinema.service.jsx
@@ -1,48 +1,51 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Function to get the access token from localStorage
-const getAccessToken = () => localStorage.getItem('accessToken');
+const getAccessToken = () => localStorage.getItem("accessToken");
 
 // Create the API with Redux Toolkit Query
 export const cinemaApi = createApi({
-  reducerPath: 'cinemaApi',
+  reducerPath: "cinemaApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:4003/', // Your API base URL
+    baseUrl: "http://localhost:4003/", // Your API base URL
     prepareHeaders: (headers) => {
       // Get token from localStorage
       const token = getAccessToken();
       // If token exists, add it to the Authorization header
       if (token) {
-        headers.set('Authorization', `Bearer ${token}`);
+        headers.set("Authorization", `Bearer ${token}`);
       }
       return headers;
     },
   }),
   endpoints: (builder) => ({
     getAllCinemas: builder.query({
-      query: () => '/api/cinema',
+      query: () => "/api/cinema",
     }),
     getCinemaById: builder.query({
       query: (id) => `/api/cinema/${id}`,
     }),
+    getCinemasByRegionId: builder.query({
+      query: (id) => `/api/cinema/region/${id}`,
+    }),
     addCinema: builder.mutation({
       query: (newCinema) => ({
-        url: '/api/cinema',
-        method: 'POST',
+        url: "/api/cinema",
+        method: "POST",
         body: newCinema,
       }),
     }),
     updateCinema: builder.mutation({
       query: ({ id, updatedData }) => ({
         url: `/api/cinema/${id}`,
-        method: 'PATCH',
+        method: "PATCH",
         body: updatedData,
       }),
     }),
     deleteCinema: builder.mutation({
       query: (id) => ({
         url: `/api/cinema/${id}`,
-        method: 'DELETE',
+        method: "DELETE",
       }),
     }),
   }),
@@ -50,9 +53,10 @@ export const cinemaApi = createApi({
 
 // Export the hooks to use in components
 export const {
-    useGetAllCinemasQuery,
-    useGetCinemaByIdQuery,
-    useAddCinemaMutation,
-    useUpdateCinemaMutation,
-    useDeleteCinemaMutation,
+  useGetAllCinemasQuery,
+  useGetCinemaByIdQuery,
+  useGetCinemasByRegionIdQuery,
+  useAddCinemaMutation,
+  useUpdateCinemaMutation,
+  useDeleteCinemaMutation,
 } = cinemaApi;
