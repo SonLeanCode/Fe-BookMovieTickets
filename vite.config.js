ecbbffb430800ff5f27de5507077d9ea869ea294
duy <vdntduy0410@gmail.com>
2024-12-14 23:43:14 +0700

    feat: proxy config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+  '/efx': {
+    target: 'http://localhost:4003',
+    changeOrigin: true, // Thay đổi Origin của request thành server đích
+    secure: false, // Bỏ kiểm tra SSL
+    rewrite: (path) => path.replace(/^\/efx/, ''), // Loại bỏ tiền tố '/efx' nếu không cần
+  },
+},
+
     host: '0.0.0.0',  // Lấy giá trị từ biến môi trường
     open: true,  // Tự động mở trình duyệt
-    port: 5173,  // Bạn có thể thay đổi cổng nếu cần
   },
   build: {
     sourcemap: true,  // Tạo sourcemap để debug
