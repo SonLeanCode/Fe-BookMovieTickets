964f8b92f22ec3223c1797a44344850ae9f6aff8
SonLeanCode <12c3tvknguyenvanson@gmail.com>
2024-12-15 09:36:47 +0700

    del message

--- src/pages/BuyTickets/BuyTickets.jsx
+++ src/pages/BuyTickets/BuyTickets.jsx
@@ -146,15 +146,16 @@ const BuyTickets = () => {
     const amount = parseInt(urlParams.get("amount"));
     const resultCode = urlParams.get("resultCode");
     const responseTime = urlParams.get("responseTime");
-    const message = urlParams.get("message");
-
+    // const message = urlParams.get("message");
     // Kiểm tra kết quả thanh toán
-    if (resultCode === "0" && message === "Successful.") {
+    if (resultCode === "0") {
       const paymentMethod = partnerCode; // Vì đây là MoMo, phương thức thanh toán sẽ là MoMo
       const totalAmount = amount;
       const paymentDate = new Date(parseInt(responseTime)).toLocaleString(); // Chuyển thời gian sang dạng chuỗi
       // Tạo paymentData với các tham số từ URL
-      await handleAddTicket(paymentMethod, totalAmount, paymentDate);
+     const dataMomo =  await handleAddTicket(paymentMethod, totalAmount, paymentDate);
+     console.log('datamMOMO',dataMomo)
+      
     } else {
       alert("Thanh toán không thành công. Vui lòng thử lại!");
     }
